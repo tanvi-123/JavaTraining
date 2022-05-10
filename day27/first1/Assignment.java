fb5bfb6e0d285be90975bc3987c37436006ee75c
tanvi-123 <54523728+tanvi-123@users.noreply.github.com>
2022-05-10 16:45:47 +0530

    initial

--- day27/first1/Assignment.java
+++ day27/first1/Assignment.java
@@ -2,50 +2,77 @@ package first1;
 
 import java.sql.*;
 import java.util.*;
+import java.lang.*;
 
 public class Assignment {
-    public static void main(String[] args) throws Exception {
-        // Load database driver in memory
+    public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         Class.forName("org.postgresql.Driver");
-        System.out.println("Driver loaded");
         Connection conn;
         conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/demo", "postgres", "goje");
-        System.out.println("Coonection done");
+
+        int ch;
         Statement stmt;
         stmt = conn.createStatement();
-
-        System.out.println("Enter 1 For INSERT 2 for DELETE 3 for UPDATE 4 for SELECT 5 for EXIT");
-        int choice;
-        System.out.println("Enter Your Choice");
-        choice = sc.nextInt();
-        System.out.println("Enter Id");
-        int id = sc.nextInt();
-        System.out.println("Enter Name");
-        String name = sc.next();
         String sql;
-        if (choice == 1) {
-            sql = "insert into student values(" + id + ",'" + name + "')";
-            stmt.executeUpdate(sql);
-            System.out.println("INSERTED");
-        } else if (choice == 2) {
-            sql = "delete from student where id=" + id + "";
-            stmt.executeUpdate(sql);
-            System.out.println("DELETED");
-        } else if (choice == 3) {
-            sql = "update student set id=" + id + " where name=" + name + "";
-            stmt.executeUpdate(sql);
-            System.out.println("UPDATED");
-        } else if (choice == 4) {
-            sql = "select * from student";
-            stmt.executeUpdate(sql);
-            System.out.println("SELECTED");
-        } else {
-            System.out.println("Invalid");
+        while (true) {
+
+            System.out.println("Which operation Do you want to perform?");
+            System.out.println(" 1- for Insert");
+            System.out.println(" 2- for Delete");
+            System.out.println("3- for Update");
+            System.out.println("4- for select");
+            System.out.println("5- for Exit");
+            System.out.println("Please choose your operation");
+            ch = sc.nextInt();
+            switch (ch) {
+                case 1:
+                    System.out.println(" INSERT ");
+                    System.out.println(" Id: ");
+                    int id = sc.nextInt();
+                    System.out.println(" Name: ");
+                    String name = sc.next();
+                    sql = "insert into Student values(" + id + ",'" + name + "')";
+                    stmt.executeUpdate(sql);
+                    System.out.println(" INSERTED ");
+                    break;
+
+                case 2:
+                    System.out.println("DELETE Query");
+                    System.out.println("Enter id  ");
+                    int id1 = sc.nextInt();
+                    sql = "delete from Student where id=" + id1 + "";
+                    stmt.execute(sql);
+                    System.out.println(" DELETED ");
+                    break;
+
+                case 3:
+                    System.out.println("Firing UPADATE query");
+                    System.out.println("Enter ID  ");
+                    int id2 = sc.nextInt();
+                    sql = "update Student set name='ram'where id=" + id2 + "";
+                    stmt.executeUpdate(sql);
+                    System.out.println(" UPDATED ");
+                    break;
+
+                case 4:
+                    System.out.println(" SELECT Query");
+                    System.out.println("Enter ID to select Record");
+                    int id3 = sc.nextInt();
+                    sql = "select id,name from Student where id=" + id3 + "";
+                    System.out.println(sql);
+                    break;
+
+                case 5:
+                    System.out.println("EXIT");
+                    System.exit(0);
+
+                default:
+                    System.out.println("INVALID");
+                    break;
+            }
 
         }
-
-        conn.close();
 
     }
 }
