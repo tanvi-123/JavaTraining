35e22b6339509671f1d8ca1ee3934a5cb3d15ec5
tanvi-123 <54523728+tanvi-123@users.noreply.github.com>
2022-05-20 13:03:26 +0530

    initial commit

---
+++ MVC_app/demomvc1/src/main/java/com/example/demomvc1/Demomvc1Application.java
@@ -0,0 +1,13 @@
+package com.example.demomvc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demomvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demomvc1Application.class, args);
+	}
+
+}
