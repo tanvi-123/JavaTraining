dd539c48075c2d4e95519d1bb1c1ebe76d1935f1
tanvi-123 <54523728+tanvi-123@users.noreply.github.com>
2022-05-31 15:39:01 +0530

    initial commit

---
+++ session/day45/democart/src/main/java/com/example/democart/Item.java
@@ -0,0 +1,26 @@
+package com.example.democart;
+
+public class Item {
+	
+ private String name;
+ private int cost;
+ 
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getCost() {
+	return cost;
+}
+public void setCost(int cost) {
+	this.cost = cost;
+}
+public Item(String name, int cost) {
+	super();
+	this.name = name;
+	this.cost = cost;
+}
+ 
+}
